31479c3b10eb759b56487d13f257131477a10498
Sam Boling <charles.samuel.boling@gmail.com>
2017-06-18 23:51:18 -0600

    simplified inverse z transform using series expansion at infinity

--- stuffbuf/parse/ztransform.py
+++ stuffbuf/parse/ztransform.py
@@ -1,30 +1,15 @@
 import logging
 import operator
 
-from sympy import sympify, solve
-from sympy.abc import w, z
-from sympy.functions import Abs
-from sympy.series.residues import residue
-
-# from stuffbuf.parse.exceptions import ParsingError
+from sympy import sympify, oo
+from sympy.abc import z
 
 
 class ZTransformParser:
 
     def parse(self, s, depth=16):
-        poly = sympify(s).series(1 / z, 0, depth + 1).removeO()
-        logging.info('Laurent series: {}'.format(poly))
-
-        inv_poly = poly.subs(z, 1 / w)
-        poles = solve(inv_poly, w)
-
-        coeffs = [
-            sum(
-                int(residue(poly * z**(k - 1), z, pole))
-                for pole in poles if Abs(pole) < 1
-            )
-            for k in range(depth, 0, -1)
-        ]
-        logging.info('recurrence coeffs: {}'.format(coeffs))
-
-        return lambda memory: sum(map(operator.mul, coeffs, memory))
+        e = sympify(s)
+        s = e.series(z, oo, depth)
+        coeffs = [s.coeff(z, n) for n in range(-depth + 1, 1)]
+        logging.info('coeffs: {}'.format(coeffs))
+        return lambda memory: int(sum(map(operator.mul, coeffs, memory)))
